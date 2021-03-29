9ad1a52b7d65923bfbe5798a7ffb04047d046cf6
honorezemagho <honorezemagho@gmail.com>
2021-03-29 21:21:20 +0100

    add blog  web routes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Livewire\ShowPost;
 Route::get('/', Posts::class)->name('welcome');
 
 Route::get('post/create', CreatePost::class)->name('create-post');
-Route::get('blog', Posts::class)->name('posts');
+Route::get('post-listing', Posts::class)->name('posts');
 Route::get('post/{slug}', ShowPost::class)->name('show-post');
 
 Route::get('/dashboard', function () {
@@ -27,3 +27,26 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::prefix('blog')->group(function () {
+    Route::prefix('api')->group(function () {
+        Route::get('posts', [\App\Http\Controllers\CanvasUiController::class, 'getPosts']);
+        Route::get('posts/{slug}', [\App\Http\Controllers\CanvasUiController::class, 'showPost'])
+             ->middleware('Canvas\Http\Middleware\Session');
+
+        Route::get('tags', [\App\Http\Controllers\CanvasUiController::class, 'getTags']);
+        Route::get('tags/{slug}', [\App\Http\Controllers\CanvasUiController::class, 'showTag']);
+        Route::get('tags/{slug}/posts', [\App\Http\Controllers\CanvasUiController::class, 'getPostsForTag']);
+
+        Route::get('topics', [\App\Http\Controllers\CanvasUiController::class, 'getTopics']);
+        Route::get('topics/{slug}', [\App\Http\Controllers\CanvasUiController::class, 'showTopic']);
+        Route::get('topics/{slug}/posts', [\App\Http\Controllers\CanvasUiController::class, 'getPostsForTopic']);
+
+        Route::get('users/{id}', [\App\Http\Controllers\CanvasUiController::class, 'showUser']);
+        Route::get('users/{id}/posts', [\App\Http\Controllers\CanvasUiController::class, 'getPostsForUser']);
+    });
+
+    Route::get('/{view?}', [\App\Http\Controllers\CanvasUiController::class, 'index'])
+         ->where('view', '(.*)')
+         ->name('canvas-ui');
+});
